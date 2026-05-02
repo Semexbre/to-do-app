154659fff1f5d7199980da5128dca1a572452d2b
Semere Abera <semex.abera@gmail.com>
2026-05-02 08:01:25 +0300

    Refactor task rendering and update form submission
    Refactored task rendering logic into updateTaskContainer function and removed redundant code from form submission handler.

--- script.js
+++ script.js
@@ -33,6 +33,24 @@ const addOrUpdateTask = () => {
   if (dataArrIndex === -1) {
     taskData.unshift(taskObj);
   }
+
+  updateTaskContainer();
+reset();
+};
+const updateTaskContainer = () =>{
+    taskData.forEach(
+    ({ id, title, date, description }) => {
+        tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" onclick="editTask(this)" class="btn">Edit</button>
+          <button type="button" onclick="deleteTask(this)" class="btn">Delete</button>
+        </div>
+      `
+    }
+  );
 };
 
 const reset = () => {
@@ -67,19 +85,7 @@ discardBtn.addEventListener("click", () => {
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  taskData.forEach(
-    ({ id, title, date, description }) => {
-        tasksContainer.innerHTML += `
-        <div class="task" id="${id}">
-          <p><strong>Title:</strong> ${title}</p>
-          <p><strong>Date:</strong> ${date}</p>
-          <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
-        </div>
-      `
-    }
-  );
 
-  reset()
+
+    addOrUpdateTask()
 });
